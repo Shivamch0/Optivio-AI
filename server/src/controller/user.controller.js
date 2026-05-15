d06b241fbfd2f71ce0364b5a3197866d0d9b42ce
Shivam <choudharyshivam0508@gmail.com>
2026-05-15 23:30:36 +0530

    Implement login, logout, and user account management functionalities

--- server/src/controller/user.controller.js
+++ server/src/controller/user.controller.js
@@ -67,24 +67,210 @@ const registerUser = asyncHandler(async (req, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 
-  return res.status(200)
-            .cookie("accessToken" , accessToken , options)
-            .cookie("refreshToken" , refreshToken , options)
-            .json(new ApiResponse(200 , { user : createdUser} , "User Created Successfully..."))
-
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        { user: createdUser },
+        "User Created Successfully...",
+      ),
+    );
 });
 
-const loginUser = asyncHandler(async (req, res) => {});
+const loginUser = asyncHandler(async (req, res) => {
+  // get data from req.body
+  const { userName, email, password } = req.body;
+  if ((!email && !userName) || !password) {
+    throw new ApiError(400, "Username or email is required...");
+  }
 
-const logoutUser = asyncHandler(async (req, res) => {});
+  //find the user
+  const user = await User.findOne({
+    $or: [{ userName }, { email }],
+  });
+  if (!user) {
+    throw new ApiError(
+      404,
+      "User with this email or Username does not exists...",
+    );
+  }
 
-const getCurrentUser = asyncHandler(async (req, res) => {});
+  // password check
+  const checkPassword = await user.isPasswordCorrect(password);
+  if (!checkPassword) {
+    throw new ApiError(401, "Incorrect Password...");
+  }
 
-const refreshAccessToken = asyncHandler(async (req, res) => {});
+  // generate accesstoken and refreshToken
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id,
+  );
 
-const changeCurrentPassword = asyncHandler(async (req, res) => {});
+  const loggedInUser = await User.findById(user._id).select(
+    " -password -refreshToken ",
+  );
 
-const updateAccountDetails = asyncHandler(async (req, res) => {});
+  if (!loggedInUser) {
+    throw new ApiError(400, "Something went wrong while logging...");
+  }
+
+  const options = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "Lax",
+    path: "/",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  };
+
+  // send response
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+        },
+        "User Logged in Successfully...",
+      ),
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    },
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "Lax",
+    path: "/",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User Logged Out successfully..."));
+});
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, req.user, "Current user fetched successfully..."),
+    );
+});
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const incommingRefreshToken =
+    req.cookies.refreshToken || req.body.refreshToken;
+  if (!incommingRefreshToken) {
+    throw new ApiError(401, "Unauthorized request...");
+  }
+
+  try {
+    const decodedToken = jwt.verify(
+      incommingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET,
+    );
+
+    const user = await User.findById(decodedToken?._id);
+    if (!user) {
+      throw new ApiError(401, "Invalid request token...");
+    }
+
+    if (incommingRefreshToken !== user?.refreshToken) {
+      throw new ApiError(401, "Refresh Token is expired...");
+    }
+
+    const options = {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "Lax",
+      path: "/",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    };
+
+    const { accessToken, refreshToken: newRefreshToken } =
+      await generateAccessAndRefreshToken(user._id);
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", newRefreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          { accessToken, refreshToken: newRefreshToken },
+          "Access Token is refreshed successfully...",
+        ),
+      );
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid Refresh Token");
+  }
+});
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  if (!oldPassword || !newPassword) {
+    throw new ApiError(400, "Both old and new passwords are required");
+  }
+
+  const user = await User.findById(req.user?._id);
+
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, "Invalid Old Password...");
+  }
+
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully..."));
+});
+
+const updateAccountDetails = asyncHandler(async (req, res) => {
+  const { userName, email } = req.body;
+
+  if (!userName || !email) {
+    throw new ApiError(400, "All fields are required...");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        userName,
+        email,
+      },
+    },
+    { new: true },
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, user, "Account details updated successfully..."),
+    );
+});
 
 export {
   registerUser,
