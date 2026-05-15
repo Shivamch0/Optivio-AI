6cbc30964a5b78ab65a83a4fc24210655a1891c8
Shivam <choudharyshivam0508@gmail.com>
2026-05-16 00:16:01 +0530

    feat: Add Landing page component and update routing in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,15 @@ import { Routes, Route } from "react-router-dom";
 
 import LoginPage from "./pages/Auth/LoginPage";
 import SignUpPage from "./pages/Auth/SignUpPage";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
     <>
       <div className="min-h-screen bg-[#0f1117] flex items-center justify-center p-4">
         <Routes>
+
+          <Route path="/" element={<Landing />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
         </Routes>
