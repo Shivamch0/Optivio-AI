c6efaad286518a58759ff4365129e1cdea06487c
Shivam <choudharyshivam0508@gmail.com>
2026-05-15 23:45:36 +0530

    feat: Add formik dependency and update SocialButton component to accept text prop

--- client/src/components/Button/SocialButton.jsx
+++ client/src/components/Button/SocialButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function SocialButton() {
+function SocialButton({text}) {
   return (
     <button className="w-full border border-gray-300 bg-white rounded-xl py-3 font-medium text-gray-700 hover:bg-gray-50 transition">
       {text}
