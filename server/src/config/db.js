8c171b2e0863b8e9d774757c199d3d571dafd652
Shivam <choudharyshivam0508@gmail.com>
2026-05-16 17:02:34 +0530

    refactor: Update database connection logic; set DNS servers and enhance error handling

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose';
 import { DB_NAME } from '../constant.js';
-import dns from 'dns';
-
-dns.setServers(["1.1.1.1" , "8.8.8.8"]);
 
 const connectDB = async () => {
     try {
-        const con = await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`)
+        const con = await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`, {
+            serverSelectionTimeoutMS: 5000,
+        })
+        console.log(`MongoDB connected: ${con.connection.host}`);
     } catch (error) {
         console.log("Something went wrong while connecting Database..." , error)
+        throw error;
     }
 }
 
